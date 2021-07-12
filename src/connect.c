008c9e15e82fc4395f7a03a334627ed0aadcaa0a
Thomas Noll <tn@fraachmisch.net>
2021-07-12 14:24:48 +0200

    check for ifa_addr != NULL in result of getifaddrs(3)

--- src/connect.c
+++ src/connect.c
@@ -28,7 +28,8 @@ extern int connect(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)
 		struct sockaddr_in6 *bestaddr = NULL;
 		struct sockaddr_in6 *curr = NULL;
 		while (next != NULL) {
-			if (next->ifa_addr->sa_family == AF_INET6) {
+			if (next->ifa_addr != NULL &&
+			    next->ifa_addr->sa_family == AF_INET6) {
 				curr = (struct sockaddr_in6 *)next->ifa_addr;
 				int score = oas_address_score(
 					curr->sin6_addr.s6_addr);
