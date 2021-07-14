bd999da9d1322238b11f2e830c1c7099d091e91c
Thomas Noll <tn@fraachmisch.net>
2021-07-14 11:38:00 +0200

    check success of getifaddrs and freeifaddrs

--- src/connect.c
+++ src/connect.c
@@ -22,39 +22,48 @@ extern int connect(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)
 			  (const void *restrict)(&(target6->sin6_addr)),
 			  targetaddrtext, 50);
 		struct ifaddrs *iflist;
-		getifaddrs(&iflist);
-		struct ifaddrs *next = iflist;
-		int bestscore = 10000;
-		struct sockaddr_in6 *bestaddr = NULL;
-		struct sockaddr_in6 *curr = NULL;
-		while (next != NULL) {
-			if (next->ifa_addr != NULL &&
-			    next->ifa_addr->sa_family == AF_INET6) {
-				curr = (struct sockaddr_in6 *)next->ifa_addr;
-				int score = oas_address_score(
-					curr->sin6_addr.s6_addr);
-				if (score < bestscore) {
-					bestscore = score;
-					bestaddr = curr;
-					if (bestscore == 1)
-						break; // no better match possible
+		if (getifaddrs(&iflist) == -1) {
+			syslog(LOG_WARNING,
+			       "could not retrieve available addresses");
+		} else {
+			struct ifaddrs *next = iflist;
+			int bestscore = 10000;
+			struct sockaddr_in6 *bestaddr = NULL;
+			struct sockaddr_in6 *curr = NULL;
+			while (next != NULL) {
+				if (next->ifa_addr != NULL &&
+				    next->ifa_addr->sa_family == AF_INET6) {
+					curr = (struct sockaddr_in6 *)
+						       next->ifa_addr;
+					int score = oas_address_score(
+						curr->sin6_addr.s6_addr);
+					if (score < bestscore) {
+						bestscore = score;
+						bestaddr = curr;
+						if (bestscore == 1)
+							break; // no better match possible
+					}
 				}
+				next = next->ifa_next;
 			}
-			next = next->ifa_next;
-		}
-		// try to bind if address found
-		if (bestscore < 10000 && bestaddr != NULL) {
-			inet_ntop(
-				AF_INET6,
-				(const void *restrict)(&(bestaddr->sin6_addr)),
-				sourceaddrtext, 50);
-			syslog(LOG_NOTICE,
-			       "oas selected %s as source to connect to %s",
-			       sourceaddrtext, targetaddrtext);
-			int br = bind(__fd, ((const struct sockaddr *)bestaddr),
-				      sizeof(struct sockaddr_in6));
-			syslog(LOG_DEBUG, "bind returns: %d with errno: %d", br,
-			       errno);
+			// try to bind if address found
+			if (bestscore < 10000 && bestaddr != NULL) {
+				inet_ntop(AF_INET6,
+					  (const void *restrict)(
+						  &(bestaddr->sin6_addr)),
+					  sourceaddrtext, 50);
+				syslog(LOG_NOTICE,
+				       "oas selected %s as source to connect to %s",
+				       sourceaddrtext, targetaddrtext);
+				int br = bind(
+					__fd,
+					((const struct sockaddr *)bestaddr),
+					sizeof(struct sockaddr_in6));
+				syslog(LOG_DEBUG,
+				       "bind returns: %d with errno: %d", br,
+				       errno);
+			}
+			freeifaddrs(iflist);
 		}
 	}
 #ifndef DEBUG
